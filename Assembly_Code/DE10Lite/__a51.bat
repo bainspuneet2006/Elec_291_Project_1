61301e16cf271158aad0f5ac18236c0849edbada
Suleman <ahmadsuleman726@gmail.com>
2026-02-10 21:53:04 -0700

    Newest bestest code ever

--- Assembly_Code/DE10Lite/__a51.bat
+++ Assembly_Code/DE10Lite/__a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\elec291\Elec_291_Project_1\Assembly_Code\DE10Lite\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\elec291\Elec_291_Project_1\Assembly_Code\DE10Lite\PROJECT_CODE_TEMP_working.asm"
-echo Crosside_Action Set_Hex_File C:\elec291\Elec_291_Project_1\Assembly_Code\DE10Lite\PROJECT_CODE_TEMP_working.HEX
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\elec291\Elec_291_Project_1\Assembly_Code\DE10Lite\jodhan_testing_full.asm"
+echo Crosside_Action Set_Hex_File C:\elec291\Elec_291_Project_1\Assembly_Code\DE10Lite\jodhan_testing_full.HEX
